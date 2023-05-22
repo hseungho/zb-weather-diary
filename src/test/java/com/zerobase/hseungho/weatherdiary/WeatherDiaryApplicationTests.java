fd4beb960c83c8d47f089be942d36b895460dbc6
hseungho <hsh47607@naver.com>
2023-05-23 01:54:51 +0900

    init test code

--- src/test/java/com/zerobase/hseungho/weatherdiary/WeatherDiaryApplicationTests.java
+++ src/test/java/com/zerobase/hseungho/weatherdiary/WeatherDiaryApplicationTests.java
@@ -3,11 +3,24 @@ package com.zerobase.hseungho.weatherdiary;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class WeatherDiaryApplicationTests {
 
     @Test
-    void contextLoads() {
+    void equalTest() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void nullTest() {
+        assertNull(null);
+    }
+
+    @Test
+    void trueTest() {
+        assertTrue(1==1);
     }
 
 }
