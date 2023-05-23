f90d9ecd3a21a7b6d5cc66e8606d50c3c33a7d00
hseungho <hsh47607@naver.com>
2023-05-23 20:43:11 +0900

    feat: get weather data from api

--- src/main/java/com/zerobase/hseungho/weatherdiary/service/DiaryService.java
+++ src/main/java/com/zerobase/hseungho/weatherdiary/service/DiaryService.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 
 @Service
@@ -14,12 +18,33 @@ public class DiaryService {
     private String apiKey;
 
     public void createDiary(LocalDate date, String text) {
-
+        String weatherData = getWeatherString();
     }
 
     private String getWeatherString() {
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid="+apiKey;
-        return "";
+        try {
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int code = connection.getResponseCode();
+            BufferedReader br;
+            if (code == 200) {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            return response.toString();
+        } catch (Exception e) {
+            return "failed to get response";
+        }
     }
 
 }
